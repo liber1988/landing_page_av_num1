63556dd303e5a255a41c803fb1b4961768f7e408
liber1988 <92869703+liber1988@users.noreply.github.com>
2026-07-08 17:07:26 +0300

    AOS issue fix

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,18 +1,24 @@
 "use client";
-import AOS, { init } from "aos";
-import "aos/dist/aos.css";
+
 import React, { useEffect } from "react";
+
 import Hero from "./Hero/Hero";
 import WhyChoose from "./WhyChoose/WhyChoose";
-
 import AnalyticsFeature from "./AnalyticsFeature/AnakyticsFeature";
 import Feature from "./Feature/Feature";
 import Review from "./Review/Review";
 import Price from "./Price/Price";
+
 function Home() {
   useEffect(() => {
-    const initAOS = async () => {
-      await import("aos");
+    const loadAOS = async () => {
+      const AOS = (await import("aos")).default;
+
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = "https://unpkg.com/aos@2.3.4/dist/aos.css";
+      document.head.appendChild(link);
+
       AOS.init({
         duration: 1000,
         easing: "ease-in-out",
@@ -20,14 +26,17 @@ function Home() {
         anchorPlacement: "top-bottom",
       });
     };
-    initAOS();
+
+    loadAOS();
   }, []);
+
   return (
     <div>
       <Hero />
       <WhyChoose />
       <AnalyticsFeature />
       <Feature />
+      <Review />
       <Review />
       <Price />
     </div>
